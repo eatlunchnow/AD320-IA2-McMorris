0f8ea41d768afe80e3a11a5d8fe0097c7a3db350
Kiki Mac <eatlunchnow@users.noreply.github.com>
2019-01-19 12:25:38 -0800

    Add files via upload

---
+++ formget.php
@@ -0,0 +1,14 @@
+<html>
+<body>
+	
+
+Name: <?php echo $_GET["name"]; ?><br>
+Email: <?php echo $_GET["email"]; ?><br>
+Phone number: <?php echo $_GET["number"]; ?><br>
+State of Residence: <?php echo $_GET["state"]; ?>	
+
+	
+	</body>
+</html>
+	
+	
